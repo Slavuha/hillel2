42c9e6ceb0ecc10d29228d2b19e6e75d2f34de58
Slavuha <98091309+Slavuha@users.noreply.github.com>
2022-01-22 20:20:00 +0200

    Update Main.java
    yoho88

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+yoho
         int[][] matrix = init(4);
         print(matrix);
         System.out.println();
